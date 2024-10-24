4ebd35c12616d8593a4d20cc2e8b60fc0e5b7fdc
amantur <amantur.mamatazimov04@gmail.com>
2024-10-25 04:38:06 +0600

    commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2200951303"
+    serviceWorkerVersion: "2604583367"
   }
 });
